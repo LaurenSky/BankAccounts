82c982405eb3fc1ffeb2f092c7decbe663cc8b70
LaurenSky <ljherrer@gmail.com>
2016-08-24 15:55:13 -0700

    Accnt Wave2 basic reqs completed

--- account.rb
+++ account.rb
@@ -1,33 +1,52 @@
-require_relative 'owner'
+#require_relative 'owner'
+require 'csv'
 
 module Bank
   class Account
     attr_reader :balance
 
-    def initialize (init_balance)
-      @id = rand(100..999)
+    def initialize (id, balance, open_date = Time.now)
+      @id = id
 
-      if init_balance > 0
-        @balance = init_balance
+      if balance > 0
+        @balance = balance
       else
-          raise ArgumentError, "You can't start an account with a negative balance"
+        raise ArgumentError, "You can't start an account with a negative balance"
       end
-      @owner = Bank::Owner.new(@id)
+
+      @date_opened = open_date
+      #@owner = Bank::Owner.new(@id)
+    end
+
+    # self.all - returns a collection of Account instances, representing all of the Accounts described in the CSV. See below for the CSV file specifications
+    def self.all
+      accounts = {}
+      # /Documents/Ada/Week3/BankAccounts/support/accounts.csv
+      CSV.read('support/accounts.csv').each do |line|
+        accounts[line[0]] = self.new(line[0], line[1].to_i, line[2])
+      end
+      return accounts
+    end
+
+    # self.find(id) - returns an instance of Account where the value of the id field in the CSV matches the passed parameter
+    def self.find (id)
+      accounts = self.all
+      return accounts[id]
     end
 
     def withdraw (amt_withdrawn)
       if @balance - amt_withdrawn > 0
         @balance = @balance - amt_withdrawn
-        return @balance
+        return "$#{@balance/100}"
       else
         puts "Sorry, but you do not have that amount of money in your account."
-        return @balance
+        return "$#{@balance/100}"
       end
     end
 
     def deposit (amt_deposited)
       @balance = @balance + amt_deposited
-      return @balance
+      return "$#{@balance/100}"
     end
 
   end
